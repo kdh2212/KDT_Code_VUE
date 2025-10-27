a56b4ac4e90c89d821ae8593e996932aaf117100
kdh2212 <jjun8248@gmail.com>
2025-10-27 16:52:03 +0900

    VUE : Router Example Code

---
+++ 20251027/vue01/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>홈 화면</h2>
+        <p>이 앱은 Vue Router 와 Pinia 기본 예제 입니다.</p>
+    </div>
+</template>
---
+++ 20251027/vue01/src/views/ProductsView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>상품 페이지</h2>
+        <p>장 바구니에 담긴 상품 수 : {{counter.count}}</p>
+        <button @click="counter.increment">상품 추가</button>
+        <button @click="counter.rest">리셋</button>
+    </div>
+</template>
+
+<script setup>
+import {useCounterStore} from '../stores/counter'
+const counter = useCounterStore()
+</script>
